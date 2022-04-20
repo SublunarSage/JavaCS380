3a292e63d66d2cb98627975c38b2e07cd84fc034
Isaiah Colwell <lambdacolwell@gmail.com>
2022-04-20 13:05:48 -0700

    Implement Gradle build-tool into project

--- src/main/java/edu/cwu/Main.java
+++ src/main/java/edu/cwu/Main.java
@@ -1,9 +1,7 @@
-package main.java.edu.cwu;
+package edu.cwu;
 
 public class Main {
-
     public static void main(String[] args) {
-
-        System.out.println("Hello World!");
+        System.out.println("Hello world!");
     }
 }

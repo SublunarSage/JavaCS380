e1d016100d30838e632050332b004552b9961678
Isaiah Colwell <lambdacolwell@gmail.com>
2022-05-03 22:43:11 -0700

    Hello world in LWJGL3!

--- src/main/java/edu/cwu/Main.java
+++ src/main/java/edu/cwu/Main.java
@@ -1,7 +1,9 @@
 package edu.cwu;
 
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
+        new HelloWorld().run();
     }
 }

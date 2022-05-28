e1b7bf2b6f4c35c4a72dbbcf55c0d1c1fe71ab07
Isaiah Colwell <lambdacolwell@gmail.com>
2022-05-28 13:37:24 -0700

    Implement Time class.

--- src/main/java/edu/cwu/Window.java
+++ src/main/java/edu/cwu/Window.java
@@ -1,20 +1,16 @@
 package edu.cwu;
 
+import edu.cwu.tools.Time;
 import imgui.ImGui;
-import imgui.ImGuiIO;
 import imgui.flag.ImGuiConfigFlags;
 import imgui.gl3.ImGuiImplGl3;
 import imgui.glfw.ImGuiImplGlfw;
 
-import org.lwjgl.*;
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
-import org.lwjgl.system.*;
 
-import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryStack.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
 public class Window {
@@ -30,6 +26,8 @@ public class Window {
     private final ImGuiLayer imguiLayer;
 
     private double dummyVar = 0;
+
+    private SingleChannelWave SCWave = new SingleChannelWave();
 
 
     public Window(ImGuiLayer layer) {
@@ -66,25 +64,37 @@ public class Window {
             System.exit(-1);
         }
 
-        glslVersion = "#version 130";
+        // Configure GLFW
+        glslVersion = "#version 330";
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
         glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+
+
+        // Create the Window
         windowPtr = glfwCreateWindow(WIDTH, HEIGHT, "JOSC", NULL, NULL);
 
-
         if (windowPtr == NULL) {
-            System.out.println("Unable to create window");
-            System.exit(-1);
+            throw new IllegalStateException("Failed to create the GLFW window");
         }
 
+        // Make the OpenGL context current
         glfwMakeContextCurrent(windowPtr);
+        // Enable V-Sync
         glfwSwapInterval(1);
+
+        // Make the window visible
         glfwShowWindow(windowPtr);
 
+        // This line is critical for LWJGL's interoperation with GLFW's OpenGL context,
+        // or any context that is managed externally. LWJGL detects the context that is current in the current
+        // thread, creates the GLCapabilities instance and makes the OpenGL bindings available for use.
         GL.createCapabilities();
-        glfwSetWindowSizeLimits(windowPtr, WIDTH, HEIGHT, WIDTH, HEIGHT);
+
+        glViewport(0,0,728,HEIGHT); // Where openGL will draw pixels. Conversion from normalized to pixels.
+
 
     }
 
@@ -97,18 +107,24 @@ public class Window {
     }
 
     public void run() {
+        SCWave.init();
+
+        float beginTime = Time.getTime();
+        float endTime = Time.getTime();
+
         while (!glfwWindowShouldClose(windowPtr)) {
             float dumbSine = .2f * (float) Math.sin(2 * Math.PI * dummyVar) + .3f;
+            float dumbCos = .2f * (float) Math.cos(2 * Math.PI * dummyVar) + .3f;
+            float dumberSine = .2f * (float) Math.sin(3 * Math.PI * dummyVar) + .3f;
 
-            glClearColor(0.3f - dumbSine, dumbSine, 0.3f + dumbSine, 1.0f);
+            glClearColor(0.2f + dumbCos, 0.2f + dumberSine, 0.2f + dumbSine, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT);
 
             // Put custom OpenGL Code below -----
             dummyVar += .001;
             if(dummyVar > 1) dummyVar = 0;
 
-
-
+            SCWave.draw();
             // Put custom OpenGl Code above -----
 
             // ImGui render frame begins here
@@ -135,6 +151,11 @@ public class Window {
 
             GLFW.glfwSwapBuffers(windowPtr);
             GLFW.glfwPollEvents();
+
+            // Time management
+            endTime = Time.getTime();
+            float dt = endTime - beginTime;
+            beginTime = endTime;
         }
     }
 }
